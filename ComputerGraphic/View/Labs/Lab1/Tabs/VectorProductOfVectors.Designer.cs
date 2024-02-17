0f44b7db5458232fe17591bf02af5ce83dca9660
inittose <pol.konnik@gmail.com>
2024-02-17 20:01:05 +0700

    feature(lab1): Добавил структуру проекта
    - Изменения в MainForm (Подписал нас)
    - Создал файлы, где будут хранится вкладки и другие формы

---
+++ ComputerGraphic/View/Labs/Lab1/Tabs/VectorProductOfVectors.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace ComputerGraphic.View.Labs.Lab1.Tabs
+{
+    partial class VectorProductOfVectors
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // VectorMultiple
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "VectorMultiple";
+            Size = new Size(1142, 673);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
