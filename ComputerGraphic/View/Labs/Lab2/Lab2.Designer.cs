7d7ce95f6001f19d8b5440d625968a3706400c9f
inittose <pol.konnik@gmail.com>
2024-03-28 22:08:21 +0700

    fix(lab2): Добавил все вкладки в Lab2

--- ComputerGraphic/View/Labs/Lab2/Lab2.Designer.cs
+++ ComputerGraphic/View/Labs/Lab2/Lab2.Designer.cs
@@ -34,10 +34,13 @@
             CSATabControl = new TabControl();
             CSATabPage = new TabPage();
             PCATabPage = new TabPage();
-            polygonColoringAlgorithm1 = new Tabs.PolygonColoringAlgorithm();
+            BrazenhemTabPage = new TabPage();
+            polygonColoringAlgorithm2 = new Tabs.PolygonColoringAlgorithm();
+            userControl11 = new Tabs.UserControl1();
             CSATabControl.SuspendLayout();
             CSATabPage.SuspendLayout();
             PCATabPage.SuspendLayout();
+            BrazenhemTabPage.SuspendLayout();
             SuspendLayout();
             // 
             // cohenSutherlandAlgorithmTab1
@@ -45,13 +48,14 @@
             cohenSutherlandAlgorithmTab1.Dock = DockStyle.Fill;
             cohenSutherlandAlgorithmTab1.Location = new Point(3, 3);
             cohenSutherlandAlgorithmTab1.Name = "cohenSutherlandAlgorithmTab1";
-            cohenSutherlandAlgorithmTab1.Size = new Size(857, 407);
+            cohenSutherlandAlgorithmTab1.Size = new Size(756, 432);
             cohenSutherlandAlgorithmTab1.TabIndex = 0;
             // 
             // CSATabControl
             // 
             CSATabControl.Controls.Add(CSATabPage);
             CSATabControl.Controls.Add(PCATabPage);
+            CSATabControl.Controls.Add(BrazenhemTabPage);
             CSATabControl.Dock = DockStyle.Fill;
             CSATabControl.Location = new Point(0, 0);
             CSATabControl.Name = "CSATabControl";
@@ -65,32 +69,51 @@
             CSATabPage.Location = new Point(4, 24);
             CSATabPage.Name = "CSATabPage";
             CSATabPage.Padding = new Padding(3);
-            CSATabPage.Size = new Size(863, 413);
+            CSATabPage.Size = new Size(762, 438);
             CSATabPage.TabIndex = 0;
             CSATabPage.Text = "Алгоритм Коэна-Сазерленда";
             CSATabPage.UseVisualStyleBackColor = true;
             // 
             // PCATabPage
             // 
-            PCATabPage.Controls.Add(polygonColoringAlgorithm1);
+            PCATabPage.Controls.Add(polygonColoringAlgorithm2);
             PCATabPage.Location = new Point(4, 24);
             PCATabPage.Name = "PCATabPage";
             PCATabPage.Padding = new Padding(3);
-            PCATabPage.Size = new Size(863, 413);
+            PCATabPage.Size = new Size(762, 438);
             PCATabPage.TabIndex = 1;
             PCATabPage.Text = "Алгоритм закраски многоугольника";
             PCATabPage.UseVisualStyleBackColor = true;
             // 
-            // polygonColoringAlgorithm1
+            // BrazenhemTabPage
             // 
-            polygonColoringAlgorithm1.BrushSize = 2;
-            polygonColoringAlgorithm1.BorderColor = Color.Black;
-            polygonColoringAlgorithm1.Dock = DockStyle.Fill;
-            polygonColoringAlgorithm1.FillColor = Color.Green;
-            polygonColoringAlgorithm1.Location = new Point(3, 3);
-            polygonColoringAlgorithm1.Name = "polygonColoringAlgorithm1";
-            polygonColoringAlgorithm1.Size = new Size(857, 407);
-            polygonColoringAlgorithm1.TabIndex = 0;
+            BrazenhemTabPage.Controls.Add(userControl11);
+            BrazenhemTabPage.Location = new Point(4, 24);
+            BrazenhemTabPage.Name = "BrazenhemTabPage";
+            BrazenhemTabPage.Padding = new Padding(3);
+            BrazenhemTabPage.Size = new Size(762, 438);
+            BrazenhemTabPage.TabIndex = 2;
+            BrazenhemTabPage.Text = "Алгортим Брезенхема";
+            BrazenhemTabPage.UseVisualStyleBackColor = true;
+            // 
+            // polygonColoringAlgorithm2
+            // 
+            polygonColoringAlgorithm2.BorderColor = Color.Black;
+            polygonColoringAlgorithm2.BrushSize = 3;
+            polygonColoringAlgorithm2.Dock = DockStyle.Fill;
+            polygonColoringAlgorithm2.FillColor = Color.AliceBlue;
+            polygonColoringAlgorithm2.Location = new Point(3, 3);
+            polygonColoringAlgorithm2.Name = "polygonColoringAlgorithm2";
+            polygonColoringAlgorithm2.Size = new Size(756, 432);
+            polygonColoringAlgorithm2.TabIndex = 0;
+            // 
+            // userControl11
+            // 
+            userControl11.Dock = DockStyle.Fill;
+            userControl11.Location = new Point(3, 3);
+            userControl11.Name = "userControl11";
+            userControl11.Size = new Size(756, 432);
+            userControl11.TabIndex = 0;
             // 
             // Lab2
             // 
@@ -105,6 +128,7 @@
             CSATabControl.ResumeLayout(false);
             CSATabPage.ResumeLayout(false);
             PCATabPage.ResumeLayout(false);
+            BrazenhemTabPage.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -116,5 +140,8 @@
         private TabPage CSATabPage;
         private TabPage PCATabPage;
         private Tabs.PolygonColoringAlgorithm polygonColoringAlgorithm1;
+        private TabPage BrazenhemTabPage;
+        private Tabs.PolygonColoringAlgorithm polygonColoringAlgorithm2;
+        private Tabs.UserControl1 userControl11;
     }
 }
