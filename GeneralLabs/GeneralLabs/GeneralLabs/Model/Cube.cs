2b4cf1ce1446e58e9e969e4a6212c8e1b11532a8
inittose <pol.konnik@gmail.com>
2024-04-27 22:55:10 +0700

    feature(GeneralLabs): Реализовал объект куб

---
+++ GeneralLabs/GeneralLabs/GeneralLabs/Model/Cube.cs
@@ -0,0 +1,43 @@
+﻿namespace GeneralLabs.Model
+{
+    public class Cube
+    {
+        public List<Vector3> Vectors;
+
+        public Cube()
+        {
+            Vectors = new List<Vector3>();
+            Vectors.Add(new Vector3(1, -1, -1, 1, 1, -1));
+            Vectors.Add(new Vector3(-1, -1, -1, -1, 1, -1));
+            Vectors.Add(new Vector3(-1, -1, -1, 1, -1, -1));
+            Vectors.Add(new Vector3(-1, 1, -1, 1, 1, -1));
+
+            Vectors.Add(new Vector3(1, -1, 1, 1, 1, 1));
+            Vectors.Add(new Vector3(-1, -1, 1, -1, 1, 1));
+            Vectors.Add(new Vector3(-1, -1, 1, 1, -1, 1));
+            Vectors.Add(new Vector3(-1, 1, 1, 1, 1, 1));
+
+            Vectors.Add(new Vector3(1, 1, -1, 1, 1, 1));
+            Vectors.Add(new Vector3(-1, -1, -1, -1, -1, 1));
+            Vectors.Add(new Vector3(1, -1, -1, 1, -1, 1));
+            Vectors.Add(new Vector3(-1, 1, -1, -1, 1, 1));
+        }
+
+        public Cube(List<Vector3> vectors)
+        {
+            Vectors = vectors;
+        }
+
+        public Cube Transform(TransformMatrix matrix)
+        {
+            var vectors = new List<Vector3>();
+
+            foreach (var vector in Vectors)
+            {
+                vectors.Add(vector.Transform(matrix));
+            }
+
+            return new Cube(vectors);
+        }
+    }
+}
--- GeneralLabs/GeneralLabs/GeneralLabs/Model/Vector3.cs
+++ GeneralLabs/GeneralLabs/GeneralLabs/Model/Vector3.cs
@@ -18,6 +18,12 @@
             End = end;
         }
 
+        public Vector3(float x1, float y1, float z1, float x2, float y2, float z2)
+        {
+            Start = new Point3(x1, y1, z1);
+            End = new Point3(x2, y2, z2);
+        }
+
         public Vector3 Transform(TransformMatrix matrix)
         {
             return new Vector3(Start.Transform(matrix), End.Transform(matrix));
