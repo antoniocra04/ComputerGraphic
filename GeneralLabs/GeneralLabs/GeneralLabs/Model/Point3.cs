c213283685a800e332e4790d32939b1708a25656
inittose <pol.konnik@gmail.com>
2024-04-27 22:07:09 +0700

    feature(GeneralLabs): Реализовал вспомогательные классы

---
+++ GeneralLabs/GeneralLabs/GeneralLabs/Model/Point3.cs
@@ -0,0 +1,122 @@
+﻿namespace GeneralLabs.Model
+{
+    public class Point3 : IEquatable<Point3>, ICloneable
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public float W { get; set; }
+
+        public float this[int index]
+        {
+            get
+            {
+                switch(index)
+                {
+                    case 0:
+                        return X;
+                    case 1:
+                        return Y;
+                    case 2:
+                        return Z;
+                    case 3:
+                        return W;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    case 2:
+                        Z = value;
+                        break;
+                    case 3:
+                        W = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public Point3()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+            W = 1;
+        }
+
+        public Point3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = 1;
+        }
+
+        public Point3(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        public void Unscale()
+        {
+            X /= W;
+            Y /= W;
+            Z /= W;
+            W = 1;
+        }
+
+        public Point3 Transform(TransformMatrix matrix)
+        {
+            var point = new Point3();
+
+            for (var i = 0; i < 4; i++)
+            {
+                var temp = 0f;
+                for (var col = 0; col < 4; col++)
+                {
+                    for (var row = 0; row < 4; row++)
+                    {
+                        temp += this[col] * matrix[row, col];
+                    }
+                }
+                point[i] = temp;
+            }
+
+            point.Unscale();
+
+            return point;
+        }
+
+        public bool Equals(Point3 other)
+        {
+            if (this == other)
+            {
+                return true;
+            }
+
+            return X == other.X && Y == other.Y && Z == other.Z && W == other.W;
+        }
+
+        public object Clone()
+        {
+            return new Point3(X, Y, Z, W);
+        }
+    }
+}
---
+++ GeneralLabs/GeneralLabs/GeneralLabs/Model/Vector3.cs
@@ -0,0 +1,36 @@
+﻿namespace GeneralLabs.Model
+{
+    public class Vector3 : IEquatable<Vector3>, ICloneable
+    {
+        public Point3 Start { get; set; }
+
+        public Point3 End { get; set; }
+
+        public Vector3()
+        {
+            Start = new Point3();
+            End = new Point3();
+        }
+
+        public Vector3(Point3 start, Point3 end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Vector3 Transform(TransformMatrix matrix)
+        {
+            return new Vector3(Start.Transform(matrix), End.Transform(matrix));
+        }
+
+        public bool Equals(Vector3 other)
+        {
+            return Start.Equals(other.Start);
+        }
+
+        public object Clone()
+        {
+            return new Vector3((Point3)Start.Clone(), (Point3)End.Clone());
+        }
+    }
+}
