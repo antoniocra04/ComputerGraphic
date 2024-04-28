d472c6332a823b8395a456a41818221766c265e1
inittose <pol.konnik@gmail.com>
2024-04-28 11:47:57 +0700

    feature(GeneralLabs): Отрисовал оси

--- GeneralLabs/GeneralLabs/GeneralLabs/View/Labs/Lab4.cs
+++ GeneralLabs/GeneralLabs/GeneralLabs/View/Labs/Lab4.cs
@@ -1,4 +1,5 @@
 ﻿using GeneralLabs.Model;
+using System.Drawing;
 
 namespace GeneralLabs.View.Labs
 {
@@ -72,11 +73,29 @@ namespace GeneralLabs.View.Labs
             }
         }
 
+        Model3 Osi
+        {
+            get
+            {
+                var osi = new Model3();
+                osi.Points.Add(new Point3(0, 0, 0));
+                osi.Points.Add(new Point3(1, 0, 0));
+                osi.Points.Add(new Point3(0, 1, 0));
+                osi.Points.Add(new Point3(0, 0, 1));
+                osi.Vectors.Add(new Model3.VectorIndexed(0, 1));
+                osi.Vectors.Add(new Model3.VectorIndexed(0, 2));
+                osi.Vectors.Add(new Model3.VectorIndexed(0, 3));
+                return osi;
+            }
+        }
+
         int fi { get; set; } = 0;
 
         double Fi => (double)(fi / 180f * Math.PI);
 
         Cube Cube { get; set; } = new Cube();
+
+        int Interval { get; set; } = 50;
 
         public Lab4()
         {
@@ -108,12 +127,44 @@ namespace GeneralLabs.View.Labs
                 g.DrawLine(pen, start, end);
             }
 
+            g.Dispose();
+            PictureBox.Image = Bitmap;
+        }
+
+        void DrawOsi()
+        {
+            var penSize = 2;
+            var xPen = new Pen(Color.Red, penSize);
+            var yPen = new Pen(Color.Green, penSize);
+            var zPen = new Pen(Color.Blue, penSize);
+
+            var osi = Osi.Transform(GetScaleMatrix(100)).Transform(OrthographicXOYMatrix).Transform(CenterMatrix);
+
+            var g = Graphics.FromImage(Bitmap);
+
+            g.DrawLine(
+                xPen,
+                new PointF(osi.Points[osi.Vectors[0][0]].X, osi.Points[osi.Vectors[0][0]].Y),
+                new PointF(osi.Points[osi.Vectors[0][1]].X, osi.Points[osi.Vectors[0][1]].Y));
+
+            g.DrawLine(
+                yPen,
+                new PointF(osi.Points[osi.Vectors[1][0]].X, osi.Points[osi.Vectors[1][0]].Y),
+                new PointF(osi.Points[osi.Vectors[1][1]].X, osi.Points[osi.Vectors[1][1]].Y));
+
+            g.DrawLine(
+                zPen,
+                new PointF(osi.Points[osi.Vectors[2][0]].X, osi.Points[osi.Vectors[2][0]].Y),
+                new PointF(osi.Points[osi.Vectors[2][1]].X, osi.Points[osi.Vectors[2][1]].Y));
+
+            g.Dispose();
             PictureBox.Image = Bitmap;
         }
 
         void Draw()
         {
             Clear();
+            DrawOsi();
             DrawCube();
         }
 
@@ -128,6 +179,7 @@ namespace GeneralLabs.View.Labs
         {
             Bitmap = new Bitmap(Width, Height);
             Draw();
+            Timer.Interval = Interval;
             Timer.Enabled = true;
         }
 
@@ -138,7 +190,7 @@ namespace GeneralLabs.View.Labs
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            fi = (fi + 1) % 360;
+            //fi = (fi + 1) % 360;
             Draw();
         }
     }
